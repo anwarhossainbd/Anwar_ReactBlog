bf37f196c11976d9be369aebcc8715aed07f3027
unknown <mdanwarhossain1996bd@gmail.com>
2021-09-17 07:16:35 -0600

    project Running

--- my-app/src/Components/Summary/Summary.js
+++ my-app/src/Components/Summary/Summary.js
@@ -3,13 +3,19 @@
 import React, {Component,Fragment} from 'react';
 import  {Col,Container,Row,Card} from "react-bootstrap";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import CountUp from 'react-countup';
+
+import VisibilitySensor from "react-visibility-sensor";
+
+
+
 class Summary extends Component {
 
 
 
     render() {
-
-
 
         return (
             <Fragment>
@@ -22,13 +28,31 @@ class Summary extends Component {
 
                                     <Row className="summeryText" >
                                         <Col>
-                                            <h2 className="hundred">100</h2>
+                                            <h2 className="hundred">
+                                                <CountUp start={0} end={100}  duration={1.20} >
+                                                    {({ countUpRef, start }) => (
+                                                        <VisibilitySensor onChange={start} >
+                                                            <span ref={countUpRef} />
+
+                                                        </VisibilitySensor>
+                                                    )}
+                                                </CountUp>
+                                            </h2>
                                             <h3>Total Client</h3>
                                             <hr className="bg-white w-25 pb-1 centerClass" />
                                         </Col>
 
                                         <Col >
-                                            <h2>  100</h2>
+                                            <h2>
+                                                <CountUp start={0} end={100}  duration={1.20} >
+                                                    {({ countUpRef, start }) => (
+                                                        <VisibilitySensor onChange={start} >
+                                                            <span ref={countUpRef} />
+
+                                                        </VisibilitySensor>
+                                                    )}
+                                                </CountUp>
+                                            </h2>
                                             <h3>Total Client</h3>
                                             <hr className="bg-white w-25 pb-1 centerClass" />
                                         </Col>
@@ -43,10 +67,10 @@ class Summary extends Component {
                                     <div className="cardOfSummery">
 
                                         <h3 className="headerOfCard">How I work ?</h3>
-                                        <h4 className="cardFont">  Requirement Gathering</h4>
-                                        <h4 className="cardFont" >   System Analysis</h4>
-                                        <h4 className="cardFont">   Coding Testing</h4>
-                                        <h4 className="cardFont pb-5">   Implementation</h4>
+                                        <h4 className="cardFont" > <span style={{color:"green"}}> <FontAwesomeIcon icon={faCheckCircle} /></span>  Requirement Gathering</h4>
+                                        <h4 className="cardFont" > <span style={{color:"green"}}> <FontAwesomeIcon icon={faCheckCircle} /></span>   System Analysis</h4>
+                                        <h4 className="cardFont"> <span style={{color:"green"}}> <FontAwesomeIcon icon={faCheckCircle} /></span>   Coding Testing</h4>
+                                        <h4 className="cardFont pb-5"> <span style={{color:"green"}}> <FontAwesomeIcon icon={faCheckCircle} /></span>    Implementation</h4>
 
                                     </div>
 
